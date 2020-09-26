8b4a27bbf7c8e294a3114f1a438c14b90e39e1f9
Vicky Nguyen <44889544+VickyNguyen99@users.noreply.github.com>
2020-09-26 11:46:03 -0700

    add data to db

--- server/index.py
+++ server/index.py
@@ -1,4 +1,5 @@
 import csv
+import sqlite3
 from flask import Flask, request, make_response
 from flask_cors import CORS
 
@@ -7,45 +8,40 @@ CORS(app)
 conn = sqlite3.connect('tinder.db')
 c = conn.cursor()
 
-# Create table
-c.execute('''CREATE TABLE IF NOT EXISTS profile
-    (firstName text,
-    lastName text,
-    gender text,
-    genderPreference text,
-    email text,
-    profession text,
-    lat real,
-    long real,
-    city text,
-    favoriteAnimal text,
-    favoriteMusicGenre text,
-    age int,
-    smoking text,
-    astrologicalSign text,
-    highestEducationLevel text)''')
-
 c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='profile' ''')
 
 #if the count is 1, then table exists
 if c.fetchone()[0]!=1 :
+    # Create table
+    c.execute('''CREATE TABLE IF NOT EXISTS profile
+        (firstName text,
+        lastName text,
+        gender text,
+        genderPreference text,
+        email text,
+        profession text,
+        lat real,
+        long real,
+        city text,
+        favoriteAnimal text,
+        favoriteMusicGenre text,
+        age int,
+        smoking text,
+        astrologicalSign text,
+        highestEducationLevel text)''')
+
     with open('UVEC-Fall-2020-Seed.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
-        for tup in req:
-            sqlite_insert_with_param = """INSERT INTO profile
-                (firstName,lastName,gender,genderPreference,email,profession,lat,long,city,favoriteAnimal,favoriteMusicGenre,age,smoking,astrologicalSign,highestEducationLevel) 
-                VALUES (?, ?, ?, ?, ?);"""
+            if line_count != 0:
+                sqlite_insert_with_param = """INSERT INTO profile
+                    (firstName,lastName,gender,genderPreference,email,profession,lat,long,city,favoriteAnimal,favoriteMusicGenre,age,smoking,astrologicalSign,highestEducationLevel) 
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"""
 
-            c.execute(sqlite_insert_with_param, tup)
-            conn.commit()
-
-
-def postData():
-    if request.method == 'POST':
-        req = json.loads(request.data)['data']
-
+                c.execute(sqlite_insert_with_param, (row))
+                conn.commit()
+            line_count += 1
 
 #close the connection
 conn.close()
